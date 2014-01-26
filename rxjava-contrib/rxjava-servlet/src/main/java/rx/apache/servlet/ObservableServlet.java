27fc88d9e61f1a520f349b13b6bfd51afa4b2199
Jitendra Kotamraju <jitsni@gmail.com>
2014-01-26 12:54:03 -0800

    Adding javadoc

--- rxjava-contrib/rxjava-servlet/src/main/java/rx/apache/servlet/ObservableServlet.java
+++ rxjava-contrib/rxjava-servlet/src/main/java/rx/apache/servlet/ObservableServlet.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Jitendra Kotamraju.
+ * Copyright 2013-2014 Jitendra Kotamraju.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,8 @@ import rx.util.functions.Func2;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.*;
 
 /**
@@ -32,22 +34,47 @@ import javax.servlet.*;
  */
 public class ObservableServlet {
 
+    private static final Logger LOGGER = Logger.getLogger(ObservableServlet.class.getName());
+
+    /**
+     * Observes {@link ServletInputStream}.
+     *
+     * <p>
+     * This method uses Servlet 3.1 non-blocking API callback mechanisms. When the HTTP
+     * request data becomes available to be read, the subscribed {@code Observer}'s
+     * {@link Observer#onNext onNext} method is invoked. Similarly, when all data for the
+     * HTTP request has been read, the subscribed {@code Observer}'s
+     * {@link Observer#onCompleted onCompleted} method is invoked.
+     *
+     * <p>
+     * Before calling this method, a web application must put the corresponding HTTP request
+     * into asynchronous mode.
+     *
+     * @param in servlet input stream
+     * @return Observable of ByteBuffer
+     */
     public static Observable<ByteBuffer> create(final ServletInputStream in) {
         return Observable.create(new OnSubscribeFunc<ByteBuffer>() {
             @Override
             public Subscription onSubscribe(final Observer<? super ByteBuffer> observer) {
-                ReadListener listener = new ServletReadListener(in, observer);
+                final ServletReadListener listener = new ServletReadListener(in, observer);
                 in.setReadListener(listener);
                 return new Subscription() {
                     @Override
                     public void unsubscribe() {
-                        //throw new UnsupportedOperationException();
+                        listener.unsubscribe();
                     }
                 };
             }
         });
     }
 
+    /**
+     * Observes {@link ServletOutputStream}.
+     *
+     * @param out servlet output stream
+     * @return Observable of ByteBuffer
+     */
     public static Observable<Void> create(final ServletOutputStream out) {
         return Observable.create(new OnSubscribeFunc<Void>() {
             @Override
@@ -75,7 +102,9 @@ public class ObservableServlet {
                         try {
                             byte[] b = new byte[byteBuffer.remaining()];
                             byteBuffer.get(b);
-                            System.out.println("writing byte[]");
+                            if (LOGGER.isLoggable(Level.FINE)) {
+                                LOGGER.fine("Writing ByteBuffer to ServletOutputStream");
+                            }
                             out.write(b);
                         } catch (IOException ioe) {
                             ioe.printStackTrace();
--- rxjava-contrib/rxjava-servlet/src/main/java/rx/apache/servlet/ServletReadListener.java
+++ rxjava-contrib/rxjava-servlet/src/main/java/rx/apache/servlet/ServletReadListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Jitendra Kotamraju.
+ * Copyright 2013-2014 Jitendra Kotamraju.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,15 +21,21 @@ import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * A servlet {@link ReadListener} that pushes data to an Observer
+ * A servlet {@link ReadListener} that pushes HTTP request data to an {@link Observer}
  *
  * @author Jitendra Kotamraju
  */
 class ServletReadListener implements ReadListener {
+    private static final Logger LOGGER = Logger.getLogger(ServletReadListener.class.getName());
+
     private final Observer<? super ByteBuffer> observer;
     private final ServletInputStream in;
+    // Accessed by container thread, but assigned by some other thread (hence volatile field)
+    private volatile boolean unsubscribed;
 
     ServletReadListener(ServletInputStream in, Observer<? super ByteBuffer> observer) {
         this.in = in;
@@ -44,18 +50,38 @@ class ServletReadListener implements ReadListener {
             if (len != -1) {
                 observer.onNext(ByteBuffer.wrap(buf, 0, len));
             }
-        } while(in.isReady());
+        // loop until isReady() false, otherwise container will not call onDataAvailable()
+        } while(!unsubscribed && in.isReady());
         // If isReady() false, container will call onDataAvailable()
         // when data is available.
+        if (!unsubscribed) {
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Waiting for container to notify when there is HTTP request data");
+            }
+        }
     }
 
     @Override
     public void onAllDataRead() {
-        observer.onCompleted();
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("Read all the data from ServletInputStream");
+        }
+        if (!unsubscribed) {
+            observer.onCompleted();
+        }
     }
 
     @Override
     public void onError(Throwable t) {
-        observer.onError(t);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("Error while reading the data from ServletInputStream");
+        }
+        if (!unsubscribed) {
+            observer.onError(t);
+        }
+    }
+
+    void unsubscribe() {
+        unsubscribed = true;
     }
 }
